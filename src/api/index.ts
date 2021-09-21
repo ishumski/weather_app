a9e6209ef7a48a62a628e0a0c3e263f62f48e689
Ilya Shumski <ilya.shumski@exposit-ds.com>
2021-09-21 17:21:44 +0300

    Remove requests functions to utils

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,28 +2,18 @@ import { setForecast } from "../store/action"
 
 import { CurrentLocationData, ForecastData, } from "../types/interfaces"
 
-export const getForecastByCoords = (latitude: number, longitude: number) => {
-    const currentGeolocationByCoords: string = `lattlong=${latitude},${longitude}`
-    return currentGeolocationByCoords
-}
-
-export const getForecastByCityName = (city: string) => {
-    const currentGeolocationByCity: string = `query=${city}`
-    return currentGeolocationByCity
-}
-
-export const getForecastByGeolocationUrl = (url: string) => (dispatch: any) => {
-    const apiUrl: string = `api/location/search/?${url}`
+export const getForecast = (param: string) => (dispatch: any) => {
+    const apiUrl: string = `api/location/search/?${param}`
 
     return fetch(apiUrl)
-        .then((res: any) => res.json())
+        .then((res: Response) => res.json())
         .then((data: Array<CurrentLocationData>) => {
 
             const currentWoeid: number = data[0].woeid
             const currentLocationForecast: string = `/api/location/${currentWoeid}/`
 
             fetch(currentLocationForecast)
-                .then((response: any) => response.json())
+                .then((response: Response) => response.json())
                 .then((data: ForecastData) => {
                     dispatch(setForecast(data))
                 })
---
+++ src/common/detailed-forecast-info.tsx
@@ -0,0 +1,67 @@
+import styled from 'styled-components'
+
+const ForecastInfoContainer = styled.div`
+  width: 328px;
+  height: 204px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 22px;
+  margin-bottom: 48px;
+  color: #e7e7eb;
+  background-color: #1e213a;
+`
+
+const Title = styled.h1`
+  font-family: Raleway;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+`
+const ForecastValue = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 31px;
+`
+
+const Value = styled.div`
+  font-family: Raleway;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 64px;
+  margin-right: 5px;
+`
+const Text = styled.span`
+  font-family: Raleway;
+  font-size: 40px;
+  font-weight: 600;
+`
+
+const AdditionalInfo = styled.div`
+  font-family: Raleway;
+`
+
+const DetailedForecastInfo = ({
+  title,
+  value,
+  text,
+  additionalInfo
+}: {
+  title: string
+  value: any
+  text: string
+  additionalInfo: any
+}): JSX.Element => {
+  return (
+    <ForecastInfoContainer>
+      <Title>{title}</Title>
+      <ForecastValue>
+        <Value>{value}</Value>
+        <Text>{text}</Text>
+      </ForecastValue>
+      <AdditionalInfo>{additionalInfo}</AdditionalInfo>
+    </ForecastInfoContainer>
+  )
+}
+
+export default DetailedForecastInfo
